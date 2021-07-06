ffbd1c8d7c64c299ea4a9c3a04c55bf15687eb66
Ayushjeet <mohanty.ayush9090@gmail.com>
2021-07-06 15:06:56 +0530

    player details

--- js/Game.js
+++ js/Game.js
@@ -1,6 +1,6 @@
 class Game {
   constructor(){}
-  
+
   getState(){
     var gameStateRef  = database.ref('gameState');
     gameStateRef.on("value",function(data){
@@ -15,12 +15,39 @@ class Game {
     });
   }
 
-  start(){
+  async start(){
     if(gameState === 0){
-      //player = new Player();
-      //player.getCount();
+      player = new Player();
+      var playerCountRef=await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount=playerCountRef.val();
+        player.getCount();
+
+
+      }
+      
       form = new Form()
       form.display();
     }
   }
+
+play(){
+
+form.hide();
+textSize(30);
+text("gameStart",120,100);
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
 }
--- js/Player.js
+++ js/Player.js
@@ -1,1 +1,28 @@
 
+class Player {
+    constructor(){}
+    
+    getCount(){
+      var playerCountRef  = database.ref('playerCount');
+      playerCountRef.on("value",function(data){
+         playerCountRef = data.val();
+      })
+     
+    }
+  
+    updateCount(count){
+      database.ref('/').update({
+        playerCount: count 
+      });
+    }
+  
+   update(name){
+    var playerIndex="player"+playerCount;
+    database.ref(playerIndex).set({
+        name:name
+
+    })
+
+   }
+  }
+  
